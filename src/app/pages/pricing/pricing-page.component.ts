6a3293e0b8d6d3e4503d0b8150f413bd435f7888
David Gracia <dgracia@hiberus.com>
2025-03-02 18:11:15 +0100

    PLATFORM_ID - Cliente y Server

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-pricing',
@@ -6,4 +14,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {}
+export default class PricingPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Pricing Page',
+    });
+    this.meta.updateTag({
+      name: 'og:title',
+      content: 'Pricing Page',
+    });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Fernando,Herrera,Curso,Angular,PRO',
+    });
+  }
+}
