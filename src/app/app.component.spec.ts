3efe394b3769b5928f6953a53ba803309769abf2
David Gracia <dgracia@hiberus.com>
2025-03-09 02:13:53 +0100

    AppComponent: Unit test (2)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
   let compiled: HTMLDivElement;
 
+  @Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: `<h1>Hola mundo</h1>`,
+  })
+  class NavbarComponentMock {}
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-      providers: [provideRouter([])],
-    }).compileComponents();
+    TestBed.overrideComponent(AppComponent, {
+      set: {
+        imports: [NavbarComponentMock],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      },
+    });
+
+    // ! Recomendado
+    // await TestBed.configureTestingModule({
+    //   imports: [AppComponent],
+    //   providers: [provideRouter([])],
+    // })
+    //   .overrideComponent(AppComponent, {
+    //     add: {
+    //       imports: [NavbarComponentMock],
+    //     },
+    //     remove: {
+    //       imports: [NavbarComponent],
+    //     },
+    //   })
+    // .compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
@@ -23,6 +49,7 @@ describe('AppComponent', () => {
   });
 
   it(`should render the navbar and router-outlet`, () => {
+    console.log(compiled);
     expect(compiled.querySelector('app-navbar')).toBeTruthy();
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
